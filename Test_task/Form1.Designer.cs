8994893f7259ff0ba323130f0e2a3f7b9e8b3f62
VRastrigin <VRastrigin@gmail.com>
2014-03-15 17:25:36 +0400

    second commit of the project

---
+++ Test_task/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Test_task
+{
+    partial class Find_files
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Find_files));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // treeView1
+            // 
+            resources.ApplyResources(this.treeView1, "treeView1");
+            this.treeView1.Name = "treeView1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // textBox3
+            // 
+            resources.ApplyResources(this.textBox3, "textBox3");
+            this.textBox3.Name = "textBox3";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.textBox4, "textBox4");
+            this.textBox4.Name = "textBox4";
+            // 
+            // Find_files
+            // 
+            this.AcceptButton = this.button2;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Find_files";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Test_task/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_task
+{
+    public partial class Find_files : Form
+    {
+        bool temp_stop = false;
+        int number_of_processed = 0;
+        int new_number_of_processed = 0;
+        DateTime dold = new DateTime();
+        TimeSpan sp = new TimeSpan();
+        bool timer1_exe = false;
+        bool check = false;     
+        public Find_files()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = Properties.Settings.Default.StartFolder;
+            textBox2.Text = Properties.Settings.Default.MaskFind;
+            textBox3.Text = Properties.Settings.Default.InFileFind; 
+
+            if (textBox1.Text == "" && textBox2.Text == "" && textBox3.Text == "")
+            {
+                button4.Enabled = false;
+            }
+            else
+            {
+                button4.Enabled = true;
+            }            
+            if (textBox1.Text == "")
+            {
+                button2.Enabled = false;
+            }
+            else if (textBox1.Text != "")
+            {
+                button2.Enabled = true;
+            }
+            button3.Enabled = false;
+        }
+
+        private void FileSearchFunction(string Dir, TreeNode rootNode)             // http://blog.foolsoft.ru/c-perebor-vsex-fajlov-v-direktorii-s-uchetom-poddirektorij-i-otobrazheniem-tekushhego-fajla/
+        {                                                                          // http://msdn.microsoft.com/en-us/library/ms171645.aspx
+            DirectoryInfo DI = new DirectoryInfo((string)Dir);
+            DirectoryInfo[] SubDir = null;
+            try
+            {
+                SubDir = DI.GetDirectories();
+            }
+            catch
+            {
+                return;
+            }
+            TreeNode[] rootNode2 = new TreeNode[SubDir.Length];
+            for (int i = 0; i < SubDir.Length; ++i)
+            {
+                this.Invoke(new System.Threading.ThreadStart(delegate
+                {
+                rootNode2[i] = new TreeNode(Path.GetFileName(SubDir[i].FullName));                
+                rootNode.Nodes.Add(rootNode2[i]);
+                this.FileSearchFunction(SubDir[i].FullName, rootNode2[i]);
+                }));
+            }
+            FileInfo[] FI = DI.GetFiles();
+            for (int i = 0; i < FI.Length; ++i)
+            {
+                //Thread.Sleep(100);
+                if (temp_stop != true)
+                {
+                    this.Invoke(new System.Threading.ThreadStart(delegate
+                    {
+                        label1.Text = FI[i].FullName;
+                        string value = FI[i].FullName;
+                        string fname = Path.GetFileName(value);                        
+                        bool GoodFile = true;
+                        if (textBox2.Text != "")
+                        {
+                            GoodFile = PatternMatch(fname, "*" + textBox2.Text + "*");
+                        }
+                        else if (textBox2.Text == "")
+                        {
+                            GoodFile = true;
+                        }
+                        if (textBox3.Text == "")
+                        {
+                            GoodFile = GoodFile;
+                        }
+                        else if (textBox3.Text != "" && GoodFile == true)
+                        {
+                            GoodFile = (File.ReadAllText(value, Encoding.Default).Contains(textBox3.Text));
+                        }
+                        if (GoodFile == true)
+                        {
+                            string[] words = value.Split('\\');
+                            TreeNode aNode = new TreeNode(fname);
+                            aNode.BackColor = Color.Azure;
+                            rootNode.Nodes.Add(aNode);                  
+                        }
+                        number_of_processed++;
+                        label3.Text = number_of_processed.ToString();
+                        sp = DateTime.Now - dold;
+                    }));
+                }
+            }          
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = FBD.SelectedPath;
+                button2.Enabled = true;
+                button4.Enabled = true;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = true;
+            button4.Enabled = false;
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox3.Enabled = false;
+            treeView1.Nodes.Clear();
+            dold = DateTime.Now; 
+            timer1.Start();
+            timer1_exe = true;                               
+            temp_stop = false;
+            label1.Text = "";
+            number_of_processed = 0;
+            check = false;
+            if (textBox1.Text != String.Empty && Directory.Exists(textBox1.Text))
+            {
+                TreeNode MainRootNode;
+                MainRootNode = new TreeNode(textBox1.Text);
+                //System.Threading.Thread T = new System.Threading.Thread(new System.Threading.ParameterizedThreadStart(FileSearchFunction));              
+                //T.Start(textBox1.Text);
+                System.Threading.Thread T = new System.Threading.Thread(delegate() { FileSearchFunction(textBox1.Text, MainRootNode); });
+                T.Start();
+                treeView1.Nodes.Add(MainRootNode);
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button3.Enabled = false;
+            button4.Enabled = true;            
+            textBox2.Enabled = true;
+            textBox3.Enabled = true;
+            temp_stop = true;
+            timer1.Stop();
+            timer1_exe = false;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {           
+            button4.Enabled = false;
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            label1.Text = "-";
+            label3.Text = "0";
+            label5.Text = "-";
+            timer1_exe = false;
+            treeView1.Nodes.Clear();            
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timer1_exe == true)
+            {
+                if (label5.Text == sp.ToString(@"d\.hh\:mm\:ss\.ff"))
+                {
+                    button1.Enabled = true;
+                    button2.Enabled = true;
+                    button3.Enabled = false;
+                    button4.Enabled = true;
+                    textBox2.Enabled = true;
+                    textBox3.Enabled = true;
+                    if (check == false)
+                    {
+                        treeView1.ExpandAll();
+                        check = true;
+                    }
+                }
+                label5.Text = sp.ToString(@"d\.hh\:mm\:ss\.ff");
+                new_number_of_processed = number_of_processed;
+            }
+            else
+            {
+                timer1.Stop();
+                timer1_exe = false;
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.StartFolder = textBox1.Text;
+            Properties.Settings.Default.Save();
+            if (textBox1.Text == "")
+            {
+                button2.Enabled = false;
+            }
+            else if (textBox1.Text != "")
+            {
+                button2.Enabled = true;
+            }
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.MaskFind = textBox2.Text;
+            Properties.Settings.Default.Save();           
+            if (textBox2.Text != "")
+            {
+                button4.Enabled = true;
+            }
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.InFileFind = textBox3.Text;
+            Properties.Settings.Default.Save();
+            if (textBox3.Text != "")
+            {
+                button4.Enabled = true;
+            }
+        }
+        private bool PatternMatch(string Source, string Mask)           // http://rsdn.ru/forum/src/92822.flat#92822 
+        {
+            char[] strSource = new char[Source.Length + 1];
+            char[] strMask = new char[Mask.Length + 1];
+            Source.CopyTo(0, strSource, 0, Source.Length);
+            Mask.CopyTo(0, strMask, 0, Mask.Length);
+            int SourceIndex = 0;
+            int MaskIndex = 0;
+            for (; SourceIndex < strSource.Length && strMask[MaskIndex] != '*'; MaskIndex++, SourceIndex++)
+                if (strMask[MaskIndex] != strSource[SourceIndex] && strMask[MaskIndex] != '?')
+                    return false;
+            int pSourceIndex = 0;
+            int pMaskIndex = 0;
+            for (; ; )
+            {
+                if (strSource[SourceIndex] == 0)
+                {
+                    while (strMask[MaskIndex] == '*')
+                        MaskIndex++;
+                    return strMask[MaskIndex] == 0 ? true : false;
+                }
+                if (strMask[MaskIndex] == '*')
+                {
+                    if (strMask[++MaskIndex] == 0)
+                        return true;
+                    pMaskIndex = MaskIndex;
+                    pSourceIndex = SourceIndex + 1;
+                    continue;
+                }
+                if (strMask[MaskIndex] == strSource[SourceIndex] || strMask[MaskIndex] == '?')
+                {
+                    MaskIndex++;
+                    SourceIndex++;
+                    continue;
+                }
+                MaskIndex = pMaskIndex; SourceIndex = pSourceIndex++;
+            }
+        }
+    }
+}
